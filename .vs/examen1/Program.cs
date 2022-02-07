d80611028654534773dd22e30d49a6782f3ce9ff
Amalia Andreea Gurau <amalia.gurau@emmanuel.ro>
2022-02-07 10:35:57 +0200

    examen

---
+++ .vs/examen1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace examen1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] vector = new int[] { 6, 7, 8, 9 };
+            Console.WriteLine(suma(vector, vector.Length));
+        }
+
+        static int suma(int[] vector, int n)
+        {
+            if (n <= 0)
+                return 0;
+            return (suma(vector, n - 1) + vector[n - 1]);
+        }
+    }
+}
---
+++ .vs/examen1/Suma.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace examen1
+{
+    class Suma
+    {
+            public static int suma(int[] vector, int n)
+            {
+                if (n <= 0)
+                    return 0;
+                return (suma(vector, n - 1) + vector[n - 1]);
+            }
+    }
+}
